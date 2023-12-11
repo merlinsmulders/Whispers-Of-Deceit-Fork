e760c6c52a258fd2774c4bd369b9cb0d27781441
R4HK00N <robert.eddy.beltman@gmail.com>
2023-12-11 16:11:32 +0100

    test UI

---
+++ Assets/EigenScripts/TestUI/TestPlay.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlay : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject selectTool;
+    public GameObject selectTool2;
+    public void StartPlay()
+    {
+        player.GetComponent<OVRPlayerController>().enabled = true;
+        selectTool.SetActive(false); 
+        selectTool2.SetActive(false);
+    }
+}

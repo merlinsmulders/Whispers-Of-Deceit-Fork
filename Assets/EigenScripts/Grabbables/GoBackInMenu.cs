564b9aa41659f6fc8d03032f31456562fd66b74b
R4HK00N <robert.eddy.beltman@gmail.com>
2023-12-13 11:27:44 +0100

    UI Begin

---
+++ Assets/EigenScripts/Grabbables/GoBackInMenu.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoBackInMenu : MonoBehaviour
+{
+    public Collider handCollider;
+    public GameObject Player;
+    public GameObject selectTool;
+    public GameObject selectTool2;
+    Vector3 mainMenuPos;
+    public void Start()
+    {
+        mainMenuPos = new Vector3(-6.45f, 1.5f, 0.5f);
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.tag == "UI")
+        {
+            Player.transform.position = mainMenuPos;
+            Player.GetComponent<OVRPlayerController>().enabled = false;
+            handCollider.enabled = false;
+            selectTool.SetActive(true);
+            selectTool2.SetActive(true);
+        }
+    }
+}
--- Assets/EigenScripts/TestUI/TestPlay.cs
+++ Assets/EigenScripts/TestUI/TestPlay.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class TestPlay : MonoBehaviour
 {
+    public Collider handCollider;
     public GameObject player;
     public GameObject selectTool;
     public GameObject selectTool2;
@@ -12,5 +13,6 @@ public class TestPlay : MonoBehaviour
         player.GetComponent<OVRPlayerController>().enabled = true;
         selectTool.SetActive(false); 
         selectTool2.SetActive(false);
+        handCollider.enabled = true;
     }
 }

a3e5b439d9682775ea52186d1d8dbd8d72263000
MacKenzie1976 <97097945@st.deltion.nl>
2023-12-13 13:40:40 +0100

    UI
    main mnwuue

---
+++ Assets/Hannah/Scripts/Quit.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quit : MonoBehaviour
+{
+    
+  
+
+    
+    public void QuitApplication()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+}

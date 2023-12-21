226e5faee243bf96774edeaad6e306c7a9e4c6af
MacKenzie1976 <97097945@st.deltion.nl>
2023-12-21 13:40:19 +0100

    Lighting hallway
    Minimum adjustements

---
+++ Assets/Hannah/Scripts/LightFlicker.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour
+{
+    public Light myLight;
+    public float maxWait = 1;
+    public float maxFlicker = 0.2f;
+    float timer;
+    float interval;
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > interval)
+        {
+            ToggleLight();
+        }
+    }
+    void ToggleLight()
+    {
+        myLight.enabled = !myLight.enabled;
+        if (myLight.enabled)
+        {
+            interval = Random.Range(0, maxWait);
+        }
+        else
+        {
+            interval = Random.Range(0, maxFlicker);
+        }
+
+        timer = 0;
+    }
+}
